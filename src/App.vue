26fac109739318bae43f7e021c40192486f5965e
AkariRin <akaririn1028@gmail.com>
2022-07-02 19:12:06 +0800

    feat: 完成数据与本地同步功能

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,34 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import "katex/dist/katex.min.css";
 
 export default {
   name: "App",
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    kv: Array,
   }),
+  computed: {
+    //自动生成Latex公式
+    latex: {},
+  },
+  watch: {
+    kv: {
+      handler(newValue) {
+        localStorage.setItem("kv", JSON.stringify(newValue));
+      },
+      deep: true,
+    },
+  },
+  beforeMount() {
+    //从本地加载kv数据
+    if (localStorage.getItem("kv") != null) {
+      localStorage.setItem("kv", JSON.stringify(this.kv));
+    }
+  },
 };
 </script>
